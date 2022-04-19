54e1cb3488503d1cb49a261a328ea2f740ae24ab
Tanarys <tiffanydpro923@gmail.com>
2022-04-19 19:28:46 +0200

    first exercice done

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,34 @@
-import React from 'react'//////
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Header from './components/Header'
 import Error from './components/Error'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Header />
-      <Routes>
+      <Switch>
         <Route exact path="/">
           <Home />
         </Route>
         <Route path="/survey/:questionNumber">
           <Survey />
         </Route>
+        <Route path="/results">
+          <Results />
+        </Route>
+        <Route path="/freelances">
+          <Freelances />
+        </Route>
         <Route>
           <Error />
         </Route>
-      </Routes>
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
