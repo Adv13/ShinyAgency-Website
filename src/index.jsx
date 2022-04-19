c63c63fd83da2bba7fab5ad29b0e1e2d15229a2f
Tanarys <tiffanydpro923@gmail.com>
2022-04-19 18:54:16 +0200

    first exercice

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'//////
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Header from './components/Header'
@@ -10,7 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Header />
-      <Switch>
+      <Routes>
         <Route exact path="/">
           <Home />
         </Route>
@@ -20,7 +20,7 @@ ReactDOM.render(
         <Route>
           <Error />
         </Route>
-      </Switch>
+      </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
