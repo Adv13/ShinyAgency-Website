232611ce3e3dc8307371f0c7122f1bde88ad8e67
Adv <tiffanydpro923@gmail.com>
2022-05-02 19:26:25 +0200

    api exos with react

--- src/pages/Freelances/index.test.js
+++ src/pages/Freelances/index.test.js
@@ -1,14 +1,44 @@
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
-import { render, waitFor, screen } from '@testing-library/react'
- 
+import { waitFor, screen, waitForElementToBeRemoved } from '@testing-library/react'
 import Freelances from './'
+import { render as rtlRender } from '@testing-library/react'
+import { ThemeProvider, SurveyProvider } from '../../utils/context'
+import { MemoryRouter } from 'react-router-dom'
  
+
+const freelancersMockedData = [
+    {
+        name: 'Harry Potter',
+        job: 'Magicien frontend',
+        picture: '',
+    },
+    {
+        name: 'Hermione Granger',
+        job: 'Magicienne fullstack',
+        picture: '',
+    },
+]
+ 
+function Wrapper({ children }) {
+    return (
+        <MemoryRouter>
+            <ThemeProvider>
+                <SurveyProvider>{children}</SurveyProvider>
+            </ThemeProvider>
+        </MemoryRouter>
+    )
+}
+ 
+export function render(ui) {
+    rtlRender(ui, { wrapper: Wrapper })
+}
+
 const server = setupServer(
     // On précise ici l'url qu'il faudra "intercepter"
     rest.get('http://localhost:8000/freelances', (req, res, ctx) => {
         // Là on va pouvoir passer les datas mockées dans ce qui est retourné en json
-        return res(ctx.json({}))
+        return res(ctx.json({ freelancersList: freelancersMockedData }))
     })
 )
  
@@ -18,3 +48,13 @@ beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 // Ferme la simulation d'API une fois que les tests sont finis
 afterAll(() => server.close())
+
+it('Should display freelancers names', async () => {
+    render(<Freelances />, { wrapper: Wrapper })
+    screen.getByTestId('loader')
+    await waitForElementToBeRemoved(()=>screen.getByTestId('loader'))
+    await waitFor(()=>{
+        screen.getByText('Harry Potter')
+        screen.getByText('Hermione Granger')
+    })
+})
